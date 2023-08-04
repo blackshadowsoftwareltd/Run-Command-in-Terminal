dc3d6b9d880f103f301a890e7973709c22e6b031
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-04 21:47:59 +0600

    tabs selection

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-#![deny(clippy::all)]
-use std::process::Command;
+use modules::tab::tab_selection;
+
+pub mod modules;
 
 fn main() {
-    let flutter_command = if cfg!(target_os = "windows") {
-        "flutter.bat"
-    } else {
-        "flutter"
-    };
-
-    let project_path = "/home/remon/Rusty/";
-
-    let output = Command::new(flutter_command)
-        .current_dir(project_path)
-        .args(&["create", "project_name"])
-        .output()
-        .expect("Failed to execute Flutter command");
-
-    if output.status.success() {
-        println!("Flutter project created successfully");
-    } else {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Failed to create Flutter project: {}", stderr);
-    }
+    tab_selection();
 }
---
+++ src/modules/tab.rs
@@ -0,0 +1,44 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+pub fn tab_selection() {
+    let selections = &[
+        "Ice Cream",
+        "Vanilla Cupcake",
+        "Chocolate Muffin",
+        "A Pile of sweet, sweet mustard",
+    ];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick your flavor")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    println!("Enjoy your {}!\n\n\n\n", selections[selection]);
+
+    //? with Optional
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Optionally pick your flavor")
+        .default(0)
+        .items(&selections[..])
+        .interact_opt()
+        .unwrap();
+    if let Some(_selection) = selection {
+        println!("Enjoy your {}!\n\n\n\n", selections[_selection]);
+    } else {
+        println!("You didn't select anything!\n\n\n\n");
+    }
+
+    //? with Optional. and max 2
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Optionally pick your flavor, hint it might be on the second page")
+        .default(0)
+        .max_length(2)
+        .items(&selections[..])
+        .interact_opt()
+        .unwrap();
+    if let Some(_selection) = selection {
+        println!("Enjoy your {}!\n\n\n\n", selections[_selection]);
+    } else {
+        println!("You didn't select anything!\n\n\n\n");
+    }
+}
