f6400ee50d612d293fa45fa5eaec3b682c3b8dd0
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-04 22:58:39 +0600

    rename

--- commend-in-terminal/src/main.rs
+++ commend-in-terminal/src/main.rs
@@ -1,8 +1,26 @@
-use modules::password::password_taking;
-
-pub mod modules;
+#![deny(clippy::all)]
+use std::process::Command;
 
 fn main() {
-    // tab_selection();
-    password_taking();
+    print!("OK")
+    // let flutter_command = if cfg!(target_os = "windows") {
+    //     "flutter.bat"
+    // } else {
+    //     "flutter"
+    // };
+
+    // let project_path = "/home/remon/Rusty/";
+
+    // let output = Command::new(flutter_command)
+    //     .current_dir(project_path)
+    //     .args(&["create", "project_name"])
+    //     .output()
+    //     .expect("Failed to execute Flutter command");
+
+    // if output.status.success() {
+    //     println!("Flutter project created successfully");
+    // } else {
+    //     let stderr = String::from_utf8_lossy(&output.stderr);
+    //     eprintln!("Failed to create Flutter project: {}", stderr);
+    // }
 }
